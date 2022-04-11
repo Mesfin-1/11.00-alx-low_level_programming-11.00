52feea5cee92d7606cd8607944a7de0c9906677a
inbyflames@gmail.com <inbyflames@gmail.com>
2022-04-11 12:43:25 -0700

    with comment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-int main (void){
+/**
+ * main entry point
+ * return : o after prining function
+ * */
+int main (void)
+{
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
