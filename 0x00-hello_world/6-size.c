ecd91e64262b97cc5959d2ede9c06b1211712360
inbyflames@gmail.com <inbyflames@gmail.com>
2022-04-11 13:21:03 -0700

    final

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 int main(void)
-{
-printf("size of a char: %d bytes(s)\n", sizeof(char));
+{printf("size of a char: %d bytes(s)\n", sizeof(char));
 printf("size of an int: %d bytse(s)\n", sizeof(int));
 printf("size of a long int: %d bytes(s)\n", sizeof(long int));
 printf("size of a long long int: %d bytes(s)\n", sizeof(long long int));
