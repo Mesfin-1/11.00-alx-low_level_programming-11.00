00a11a2d6b0c6a88c55a67488bef45fd82b9e349
inbyflames@gmail.com <inbyflames@gmail.com>
2022-04-12 04:41:55 -0700

    finals

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,11 @@
+#include <main.h>
+#include < stdio.h>
+void print_to_98(int n)
+{if (n < 98)
+{for (n=n; n<98; n++)
+printf("%d, ", n);
+printf("%d", 98);}
+else 
+{for (n=n; n>98; n--)
+printf("%d, ", n);                                                         
+printf("%d", 98);}}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+#include <main.h>
+int print_last_digit(int n)
+{int last_digit;
+if (n<0)
+{last_digit = (-1 * (n % 10));
+_putchar (last_digit);}
+else
+{last_digit = (n%10);
+_putchar (last_digit + '0');
+return (last_digit);}}
