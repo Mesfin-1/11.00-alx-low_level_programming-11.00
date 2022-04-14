98285662d569afe426f6c0e3f31c3c52f298155b
inbyflames@gmail.com <inbyflames@gmail.com>
2022-04-14 03:50:27 -0700

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,21 @@
-#include "main.h"
-int _isupper(int c)
-{
-if (c >= 65 && c <= 90)
-{
-return (1);
-}
-else
-{
+#include "main.h"                                                                                                                                        
+
+int _isupper(int c)                                                                                                                                      
+
+{                                                                                                                                                        
+
+if (c >= 'A' && c <= 'Z')                                                                                                                                
+
+{                                                                                                                                                        
+
+return (1);                                                                                                                                              
+
+}                                                                                                                                                        
+
+else                                                                                                                                                     
+
+{                                                                                                                                                        
+
 return (0);
 }
 }
