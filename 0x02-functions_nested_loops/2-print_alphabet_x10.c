eeb83d9176db4b0cc5c91491aab1368a2669cc98
inbyflames@gmail.com <inbyflames@gmail.com>
2022-04-14 00:28:18 -0700

    tasks

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,11 @@
 #include "main.h"
 void print_alphabet_x10(void)
-
 {int alphabet;
 int count;
 count = 0;
 while (count < 10)
 {for (alphabet = 'a' ; alphabet <= 'z'; alphabet++)
-{_putchar(alphabet);
-}
+{ _putchar(alphabet);}
 count++;
 _putchar('\n');
 }
