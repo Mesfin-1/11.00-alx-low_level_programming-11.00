b32743746a06bbea0ddcf389a25272a547a10172
inbyflames@gmail.com <inbyflames@gmail.com>
2022-04-14 00:55:31 -0700

    committz

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,11 +1,8 @@
 #include "main.h"
 int print_last_digit(int n)
-{int last_digit;
+{int ln = n % 10;
 if (n<0)
-{last_digit = (-1 * (n % 10));
-_putchar (last_digit);}
-else
-{last_digit = (n%10);
-_putchar (last_digit + '0');
-return (last_digit);}
+ln = ln * -1;
+_putchar (ln + '0');
+return (ln); }
 }
