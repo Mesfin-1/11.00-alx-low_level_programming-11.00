0686990b70a9dd6c0780a52c10ae8b614f62ebc4
inbyflames@gmail.com <inbyflames@gmail.com>
2022-04-14 01:34:19 -0700

    task 2

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,12 @@
+#include "main.h"
+int largest_number(int a, int b, int c)
+{ int largest;
+if (a > b && a > c)
+{ largest = a; }
+else if (a > b && c > a)
+{ largest = c; }
+else if (b > c)
+{ largest - b; }
+else 
+{ largest = c; }
+return (largest); }
