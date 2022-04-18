d3ccc5be1f9bdff7b321e1d711cfa414083f22f5
inbyflames@gmail.com <inbyflames@gmail.com>
2022-04-17 22:37:33 -0700

    debug

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,31 @@
 #include "main.h"
 
-
-
+/**
+ *
+ *  * main - main
+ *
+ *   * Return: 0
+ *
+ *    */
 
 
 
 int main(void)
+
 {
 
-int i;
-i = 0;
-positive_or_negative(i);
+	int i;
 
-return (0);
+	i = 0;
+
+
+
+	positive_or_negative(i);
+
+
+
+	return (0);
+
+
+
 }
