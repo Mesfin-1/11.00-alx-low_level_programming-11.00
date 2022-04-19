b1011d26a18a85f4dad7526101b2a00c6979c6ef
inbyflames@gmail.com <inbyflames@gmail.com>
2022-04-19 15:59:15 -0700

    ponters

---
+++ 0x05-pointers_arrays_strings/main.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+/**
+ *
+ * *_putchar -writes the character c to stdout
+ *
+ * *@c: the character to print
+ *
+ * *return: on success 1
+ *
+ * * on error -1 is returned and error is there returned
+ *
+ * */
+
+int _putchar(char c)
+
+{return (write(1, &c, 1));
+}
