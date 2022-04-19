8fb9e2d12103ed31b298f402412adbf3aa794be9
inbyflames@gmail.com <inbyflames@gmail.com>
2022-04-19 16:25:49 -0700

    poninters

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *
+ *_puts prints a string
+ *@astr a pointer to an int that will be updated
+ *return: void that means is correct
+ **/
+
+void _puts(char  *str)
+
+{
+char *c;
+int m;
+c = str;
+for (m = 0; c[m]; m++)
+{
+_putchar (c[m]);
+}
+_putchar ('\n');
