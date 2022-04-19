054dd28d134a5cf3a28bd146539dc8ab3869cd84
inbyflames@gmail.com <inbyflames@gmail.com>
2022-04-19 16:13:53 -0700

    poninters

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_strlen -length of a string
+ *@s: a pointer to an int that will be updated
+ *return: void that means is correct
+ */
+
+int _strlen(char  *s)
+
+{
+int u;
+u = 0;
+while ( s[u] != ‘\0’)
+{ 
+u++;
+}
+return (u);
+}
