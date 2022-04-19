daca1ed5abdd1dd218c3131ba05bae79a7f22f1b
inbyflames@gmail.com <inbyflames@gmail.com>
2022-04-19 16:02:20 -0700

    poninters

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-  *reset_to_98 – updates a integer value to 98
+  *reset_to_98: – updates a integer value to 98
   *@n: a pointer to an int that will be updated
   *return: void that means is correct
   */
