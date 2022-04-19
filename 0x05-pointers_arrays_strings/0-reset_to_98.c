c14d7137e291faae99570c30f4b1ce3212531b50
inbyflames@gmail.com <inbyflames@gmail.com>
2022-04-19 15:46:55 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include “main.h”
+
+/**
+ *
+ * *reset_to_98 – updates a integer value to 98
+ *
+ * *@n: a pointer to an int that will be updated
+ *
+ * *return: void that means is correct
+ *
+ * */
+
+void reser_to_98(int n)
+
+{
+*n = 98
+}
+
