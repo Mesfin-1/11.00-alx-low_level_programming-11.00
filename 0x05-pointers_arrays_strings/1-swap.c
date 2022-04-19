640da1f0ed7be80287c7260957d853acf9a2ab30
inbyflames@gmail.com <inbyflames@gmail.com>
2022-04-19 16:07:17 -0700

    poninters

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include “main.h”
+
+/**
+ *swap_int  - swap the values of two integer 1 and b
+ *@a: a pointer to an int that will be updated
+ *@b: b pointer to an int that will be updated
+ *return: void that means is correct
+ */
+
+void swap_int(int *a, int *b)
+
+{
+int f;
+f = *a;
+*a = *b
+*b = f;
+}
