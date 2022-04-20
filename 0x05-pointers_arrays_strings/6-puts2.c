ebbe216595e26f419d2ceb41bfde2a13869c54c2
inbyflames@gmail.com <inbyflames@gmail.com>
2022-04-19 17:11:13 -0700

    task 7

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+
+*puts2 prints every character of a sting
+
+*@str: a pointer to an int that will be updated
+
+*return: void that means is correct
+
+*/
+
+void puts2(char *str)
+
+{
+
+int a;
+
+for (a = 0; str[a] != '\0'; a++)
+
+{
+
+if (a % 2 == 0)
+
+_putchar (str[a]);
+
+}
+
+_putchar ('\n');
+
+}
+
