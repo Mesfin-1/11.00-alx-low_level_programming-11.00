fd8878b5e8f9d71390ac204356122d8c6b02154d
inbyflames@gmail.com <inbyflames@gmail.com>
2022-04-19 18:13:39 -0700

    for task 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -20,15 +20,17 @@ printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 366 - day);
 }
 else
-{ if (day == 60 && month == 2 ) 
+{
+if (day == 60 && month == 2 ) 
+{
+printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year); 
 }
-printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year); }
-(( else
+else
 {
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 365 - day);
 }
-( }
-( }
+}
+}
 
 
