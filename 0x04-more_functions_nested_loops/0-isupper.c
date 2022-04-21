6b0ad1280416cdd1546e08b743ea5773b8c0e953
inbyflames@gmail.com <inbyflames@gmail.com>
2022-04-21 02:17:12 -0700

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,25 +3,18 @@
 
 
 /**
+ * _isupper - a function that checks for uppercase character
+ * @c: input
  *
- *  * _isupper - a function that checks for uppercase character
- *
- *   * @c: input
- *
- *    * Return: 1 if c is uppercase, 0 otherwise
- *
- *     */
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
 
 int _isupper(int c)
 
 {
 
-		if ((c >= 'A') && (c <= 'Z'))
-
-					return (1);
-
-			else
-
-						return (0);
-
+if ((c >= 'A') && (c <= 'Z'))
+return (1);
+else
+return (0);
 }
