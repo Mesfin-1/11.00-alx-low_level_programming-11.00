8efa072ab3f02cc5a45427fbff8728afe89ecc47
inbyflames@gmail.com <inbyflames@gmail.com>
2022-04-25 04:04:51 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success/correct)
+ */
+int main(void)
+{
+int i;
+for (i = 48; i<=57; i++:
+{
+putchar ('\n');
+return (0);
+}
