035efa75be6c87d3c17381ed5b61df3ba2adbe50
inbyflames@gmail.com <inbyflames@gmail.com>
2022-04-25 03:48:52 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Enrty point
+ *
+ * Return: Always  (success/correct)
+ */
+int main(void)
+{
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+putchar(alphabet);
+}
+putchar('\n');
+reteurn (0);
+}
