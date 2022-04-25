bdb60678452d4a68fbabaa4b850eaa3a07e8511d
inbyflames@gmail.com <inbyflames@gmail.com>
2022-04-25 03:52:36 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always (success/correct)
+ */
+int main(void)
+{
+char lc;
+char uc;
+for (lc = 'a'; lc <= 'z'; lc++_
+{
+putchar(lc);
+}
+for (uc = 'A'; uc <= 'Z'; uc++)
+{
+putchar(uc);
+}
+putchar('\n');
+return (0);
+}
