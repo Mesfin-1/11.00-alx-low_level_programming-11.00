c15ef377214c6604836896c7fb8cea686015b959
inbyflames@gmail.com <inbyflames@gmail.com>
2022-04-25 04:11:19 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success/correct)
+ */
+int main(void)
+{
+char alphabet;
+for (alphabet = 22; alphabet >= 97; alphabet--)
+{
+putchar(alphabet);
+}
+putchar ('\n');
+return (0);
+}
