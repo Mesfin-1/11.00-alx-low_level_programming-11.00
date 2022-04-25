1af03f82b8133d20368f145a9c33f1a104258542
inbyflames@gmail.com <inbyflames@gmail.com>
2022-04-25 04:02:44 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(succes/correct)
+ */
+int main(void)
+{
+int d;
+for (d = 0; d < 10; d++)
+{
+printf("%d", d);
+}
+printf("\n");
+return (0);
+}
