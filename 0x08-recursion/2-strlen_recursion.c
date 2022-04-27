b74294779293a8a9a06900aa78bf937c65e14b6b
inbyflames@gmail.com <inbyflames@gmail.com>
2022-04-26 23:34:04 -0700

    task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,9 +1,10 @@
 #include "main.h"
-/**_strlen_recursion - Calculate the length of a string
- * @s: the string to count
- *
- * Return: integer value
- */
+/**
+  * _strlen_recursion - Calculate the length of a string
+  * @s: the string to count
+  *
+  * Return: integer value
+  */
 int _strlen_recursion(char *s)
 
 {
