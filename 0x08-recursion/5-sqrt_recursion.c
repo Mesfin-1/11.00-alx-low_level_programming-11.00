8f205946f52bd8ee2580882e45a13ce900256860
inbyflames@gmail.com <inbyflames@gmail.com>
2022-04-27 00:06:53 -0700

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -17,12 +17,12 @@ return (__sqrt(n, 1));
   *@i: iterate number
   * 
   */
-int _sqrt(int n, int i)
+int __sqrt(int n, int i)
 {
 int sqrt = i * i;
 if (sqrt > n)
 return (-1);
 if (sqrt == n)
 return (i);
-return (_sqrt(n, i + 1));
+return (__sqrt(n, i + 1));
 }
