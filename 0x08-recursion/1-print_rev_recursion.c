d4b5f9b3e5109804c5f1e32c90cd4fa206c92481
inbyflames@gmail.com <inbyflames@gmail.com>
2022-04-26 23:26:33 -0700

    task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - Print a string in reverse
+ * @m: the string to reverse
+ *
+ * Return: Nothing.
+ */
+void _print_rev_recursion(char *m)
+
+{
+if (*m == '\0')
+{
+return;
+}
+m++;
+_print_rev_recursion();
+m--;
+_putchar(*m);
+}
